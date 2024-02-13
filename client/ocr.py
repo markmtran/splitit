c3f150ba85f3440d4bfd708e17c94b373bf4a586
markmtran <markmtran1@gmail.com>
2024-02-12 23:51:34 -0800

    formatted script

--- client/ocr.py
+++ client/ocr.py
@@ -1,2 +1,15 @@
-from easyocr import Reader
+import easyocr
+reader = easyocr.Reader(['en'], gpu=False)
 
+def extract_text(image_path):
+  result = reader.readtext(image_path)
+  return result
+
+def main():
+  img_path = "assets/test/cvs.jpg"
+  ocr_results = extract_text(img_path)
+  for text, bbox, confidence in ocr_results:
+    print(f"Text: {text}, Bounding Box: {bbox}, Confidence: {confidence}")
+
+if __name__ == "__main__":
+  main()
